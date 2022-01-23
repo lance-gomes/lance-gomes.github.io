lance-gomes/portfolio

[Portfolio] Add new website using create-react-app

src/components/About.js
@@ -1,0 +1,111 @@
+import React, { useEffect, useState } from 'react';
+
+import { 
+  AboutContainer, 
+  AboutMeDescription, 
+  SectionTitle, 
+  AboutWrapper,
+  LanceWrapper,
+  LanceImg
+} from '../styles/About';
+import { FadeContainer } from '../styles/FadeContainer';
+
+const About = () => {
+  const [shouldFadeAbout, setShouldFadeAbout] = useState(false);
+  const [shouldFadeAbout2, setShouldFadeAbout2] = useState(false);
+  const [shouldFadeAbout3, setShouldFadeAbout3] = useState(false);
+  const [shouldFadeAbout4, setShouldFadeAbout4] = useState(false);
+  const [shouldFadeAbout5, setShouldFadeAbout5] = useState(false);
+  const [shouldFadeAbout6, setShouldFadeAbout6] = useState(false);
+
+  const fadeBreakpoints = [
+    {
+      scroll: 275,
+      getter: shouldFadeAbout,
+      func: setShouldFadeAbout
+    },
+    {
+      scroll: 350,
+      getter: shouldFadeAbout2,
+      func: setShouldFadeAbout2
+    },
+    {
+      scroll: 400,
+      getter: shouldFadeAbout3,
+      func: setShouldFadeAbout3
+    },
+    {
+      scroll: 450,
+      getter: shouldFadeAbout4,
+      func: setShouldFadeAbout4
+    },
+    {
+      scroll: 500,
+      getter: shouldFadeAbout5,
+      func: setShouldFadeAbout5
+    },
+    {
+      scroll: 550,
+      getter: shouldFadeAbout6,
+      func: setShouldFadeAbout6
+    }
+  ]
+  
+  const fadeInAbout = () => { 
+    let pageYOffset = window?.pageYOffset;
+
+    fadeBreakpoints.map((breakpoint) => {
+      if (pageYOffset > breakpoint.scroll && !breakpoint.getter) {
+        breakpoint.func(true)
+      }
+    })
+  }
+  useEffect(() => {
+    fadeInAbout();
+    document.addEventListener('scroll', fadeInAbout);
+  });
+
+  return (
+    <AboutWrapper id="about">
+      <AboutContainer shouldFade={shouldFadeAbout}>
+        <FadeContainer shouldFade={shouldFadeAbout}>
+          <SectionTitle>
+            About Me
+          </SectionTitle>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout2}>
+          <AboutMeDescription>
+            Hello! 😊 My name is Lance Gomes and I am an Engineering student at the University of Waterloo
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout3}>
+          <AboutMeDescription>
+            I'm extremely passionate about <strong>software development</strong>, <strong>web development</strong>, and more recently <strong>artificial intelligence 🤖</strong>
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout4}>
+          <AboutMeDescription>
+            I have significant fullstack experience building rich and responsive web applications with ReactJS 👷🏽
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout5}>
+          <AboutMeDescription>
+            I also have over four years of experience solving a wide variety of problems using Java, Python, and C
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout6}>
+          <AboutMeDescription>
+            In my free time, I enjoy playing basketball, discovering new music, and catching up on sleep 🛌
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout6}>
+        <LanceWrapper>
+          <LanceImg/>
+        </LanceWrapper>
+        </FadeContainer>
+      </AboutContainer>
+    </AboutWrapper>
+  )
+}
+
+export default About;

src/components/SocialMedia.js
@@ -1,0 +1,33 @@
+import React from 'react';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
+import DescriptionIcon from '@material-ui/icons/Description';
+
+import { SocialMediaContainer, Link, ButtonContainer } from '../styles/SocialMedia';
+
+
+const SocialMedia = () => {
+  return (
+    <SocialMediaContainer>
+        <ButtonContainer>
+          <Link href="https://www.linkedin.com/in/lance-gomes-659766177/" target="_blank" rel="noopener noreferrer">
+            <LinkedInIcon color="primary" style={{width: '80px', height: '80px'}}/>
+          </Link>
+          <Link href="https://github.com/lance-gomes/" target="_blank" rel="noopener noreferrer">
+            <GitHubIcon style={{color: 'black', width: '70px', height: '70px', padding: '0 5px 0px 5px'}}/>
+          </Link>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Link href="mailto:lvgomes@uwaterloo.ca" target="_blank" rel="noopener noreferrer">
+            <EmailIcon style={{width: '80px', height: '80px'}}/>
+          </Link>
+          <Link href="./resume.pdf" target="_blank" rel="noopener noreferrer">
+            <DescriptionIcon style={{width: '80px', height: '80px', color: 'grey'}}/>
+          </Link>
+        </ButtonContainer>
+    </SocialMediaContainer>
+  )
+}
+
+export default SocialMedia;

lance-gomes/lance-gomes.github.io

src/components/About.js
@@ -1,0 +1,111 @@
+import React, { useEffect, useState } from 'react';
+
+import { 
+  AboutContainer, 
+  AboutMeDescription, 
+  SectionTitle, 
+  AboutWrapper,
+  LanceWrapper,
+  LanceImg
+} from '../styles/About';
+import { FadeContainer } from '../styles/FadeContainer';
+
+const About = () => {
+  const [shouldFadeAbout, setShouldFadeAbout] = useState(false);
+  const [shouldFadeAbout2, setShouldFadeAbout2] = useState(false);
+  const [shouldFadeAbout3, setShouldFadeAbout3] = useState(false);
+  const [shouldFadeAbout4, setShouldFadeAbout4] = useState(false);
+  const [shouldFadeAbout5, setShouldFadeAbout5] = useState(false);
+  const [shouldFadeAbout6, setShouldFadeAbout6] = useState(false);
+
+  const fadeBreakpoints = [
+    {
+      scroll: 275,
+      getter: shouldFadeAbout,
+      func: setShouldFadeAbout
+    },
+    {
+      scroll: 350,
+      getter: shouldFadeAbout2,
+      func: setShouldFadeAbout2
+    },
+    {
+      scroll: 400,
+      getter: shouldFadeAbout3,
+      func: setShouldFadeAbout3
+    },
+    {
+      scroll: 450,
+      getter: shouldFadeAbout4,
+      func: setShouldFadeAbout4
+    },
+    {
+      scroll: 500,
+      getter: shouldFadeAbout5,
+      func: setShouldFadeAbout5
+    },
+    {
+      scroll: 550,
+      getter: shouldFadeAbout6,
+      func: setShouldFadeAbout6
+    }
+  ]
+  
+  const fadeInAbout = () => { 
+    let pageYOffset = window?.pageYOffset;
+
+    fadeBreakpoints.map((breakpoint) => {
+      if (pageYOffset > breakpoint.scroll && !breakpoint.getter) {
+        breakpoint.func(true)
+      }
+    })
+  }
+  useEffect(() => {
+    fadeInAbout();
+    document.addEventListener('scroll', fadeInAbout);
+  });
+
+  return (
+    <AboutWrapper id="about">
+      <AboutContainer shouldFade={shouldFadeAbout}>
+        <FadeContainer shouldFade={shouldFadeAbout}>
+          <SectionTitle>
+            About Me
+          </SectionTitle>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout2}>
+          <AboutMeDescription>
+            Hello! 😊 My name is Lance Gomes and I am an Engineering grad from the University of Waterloo
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout3}>
+          <AboutMeDescription>
+            I'm extremely passionate about <strong>software development</strong>, <strong>web development</strong>, and more recently <strong>artificial intelligence 🤖</strong>
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout4}>
+          <AboutMeDescription>
+            I have significant fullstack experience building rich and responsive web applications with ReactJS 👷🏽
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout5}>
+          <AboutMeDescription>
+            I also have over five years of experience solving a wide variety of problems using Java, Python, and C
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout6}>
+          <AboutMeDescription>
+            In my free time, I enjoy playing basketball, discovering new music, and catching up on sleep 🛌
+          </AboutMeDescription>
+        </FadeContainer>
+        <FadeContainer shouldFade={shouldFadeAbout6}>
+        <LanceWrapper>
+          <LanceImg/>
+        </LanceWrapper>
+        </FadeContainer>
+      </AboutContainer>
+    </AboutWrapper>
+  )
+}
+
+export default About;

src/components/SocialMedia.js
@@ -1,0 +1,30 @@
+import React from 'react';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
+import DescriptionIcon from '@material-ui/icons/Description';
+
+import { SocialMediaContainer, Link, ButtonContainer } from '../styles/SocialMedia';
+
+
+const SocialMedia = () => {
+  return (
+    <SocialMediaContainer>
+        <ButtonContainer>
+          <Link href="https://www.linkedin.com/in/lance-gomes-659766177/" target="_blank" rel="noopener noreferrer">
+            <LinkedInIcon color="primary" style={{width: '80px', height: '80px'}}/>
+          </Link>
+          <Link href="https://github.com/lance-gomes/" target="_blank" rel="noopener noreferrer">
+            <GitHubIcon style={{color: 'black', width: '70px', height: '70px', padding: '0 5px 0px 5px'}}/>
+          </Link>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Link href="mailto:lancegomes98@gmail.com" target="_blank" rel="noopener noreferrer">
+            <EmailIcon style={{width: '80px', height: '80px'}}/>
+          </Link>
+        </ButtonContainer>
+    </SocialMediaContainer>
+  )
+}
+
+export default SocialMedia;